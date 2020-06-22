f8603d5e76915f00827ffa10dd5430e85ea6907e
Alex S <alextheprogrammer21@gmail.com>
2020-06-22 12:03:42 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
+//Created By Alex Setia
+//June 22, 2020
+//Devware
+
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import "./Search.css";
 
 function App() {
+
+  //---------------------STATES-------------------------//
+  let [searchTerm, setSearchTerm] = React.useState('');
+
+    //----------------------------FUNCTIONS-----------------------------------//
+    function handleInputChange(event) {
+      setSearchTerm(event.target.value)
+}
+
+    function searchApps(e) {
+      e.preventDefault();
+  
+      console.log(searchTerm)
+      Promise.all([
+        Promise.resolve(axios.get(`https://api.github.com/search/repositories?q=${searchTerm}`)),
+      ]).then((d) => {
+        const data = d[0].data.items[1]
+        console.log(data)
+        }) 
+      }
+        // setState(copy)
+  
+        // emailjs.send('default_service', 'template_5ul93ngj', state[0], 'user_XAbULWNSID71XKJV4cpTz')
+        // .then((result) => {
+        // }, (error) => {
+        //     alert(error.text);
+        // });
+   
+  
+
+//-----------------------HTML DISPLAY------------------------------//
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div class="wrap">
+   <div class="search">
+      <input type="text" class="searchTerm" placeholder="What are you looking for?" onChange={handleInputChange}/>
+      <button type="submit" class="searchButton" onClick={searchApps}> 
+      🔍
+        <i class="fa fa-search"></i>
+     </button>
+   </div>
+</div>
       </header>
     </div>
   );
